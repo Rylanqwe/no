1c15d42779b85a04951a2f3ad6527d71c0348438
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-02-27 19:48:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+let ticketCount = 0;
+let ticketCost = 10;
+let poolTotal = 0;
+
+function addTicket() {
+  ticketCount++;
+  updateTicketCount();
+  updatePoolTotal();
+}
+
+function addTickets(numTickets) {
+  ticketCount += numTickets;
+  updateTicketCount();
+  updatePoolTotal();
+}
+
+function updateTicketCount() {
+  document.getElementById("ticket-count").innerHTML = ticketCount;
+}
+
+function updatePoolTotal() {
+  poolTotal = ticketCount * ticketCost;
+  document.getElementById("pool-total").innerHTML = poolTotal;
+}
+
+function countdown() {
+  let drawDate = new Date("2023-03-06T00:00:00Z"); // Change this to the date and time of the next draw
+  let now = new Date();
+  let timeLeft = drawDate.getTime() - now.getTime();
+
+  let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+
+  setTimeout(countdown, 1000);
+}
+
+countdown();
+
+// Other existing code can go here
+
+
+
 // get DOM elements
 const ticketCountEl = document.getElementById('ticket-count');
 const ticketFormEl = document.getElementById('ticket-form');
