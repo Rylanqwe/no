44807c751f3ff1544532ac155498018a46392850
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-03-04 12:01:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
-let ticketCount = 0;
-let pool = [];
+// set up variables
+let ticketPrice = 1;
+let poolTotal = 0;
+let ticketsPurchased = 0;
+let tickets = [];
+let drawTime = 10; // minutes
 
-function addTicket() {
-  ticketCount++;
-  pool.push(ticketCount);
-  document.getElementById("ticketCount").innerHTML = ticketCount;
-  document.getElementById("poolSize").innerHTML = pool.length;
-}
-
-function drawWinner() {
-  if (pool.length > 0) {
-    const winner = pool[Math.floor(Math.random() * pool.length)];
-    document.getElementById("winner").innerHTML = winner;
-    pool = [];
-    ticketCount = 0;
-    document.getElementById("ticketCount").innerHTML = ticketCount;
-    document.getElementById("poolSize").innerHTML = pool.length;
+// set up functions
+function buyTickets(numTickets) {
+  // add tickets to array
+  for (let i = 0; i < numTickets; i++) {
+    tickets.push("Ticket " + (ticketsPurchased + 1 + i));
   }
+  // update tickets purchased and pool total
+  ticketsPurchased += numTickets;
+  poolTotal += numTickets * ticketPrice;
+  document.getElementById("tickets-purchased").innerHTML = ticketsPurchased;
+  document.getElementById("pool-total").innerHTML = "$" + poolTotal;
 }
 
+function clearTickets() {
+  tickets = [];
+  ticketsPurchased = 0;
+  document.getElementById("tickets-purchased").innerHTML = ticketsPurchased;
+}
+
+function updateDrawTime() {
+  let currentTime = new Date();
+  let nextDrawTime = new Date();
+  nextDrawTime.setMinutes(Math.ceil(currentTime.getMinutes() / drawTime) * drawTime);
+  nextDrawTime.setSeconds(0);
+  nextDrawTime.setMilliseconds(0);
+
+  let timeRemaining = Math.ceil((nextDrawTime.getTime() - currentTime.getTime()) / 1000);
+
+  let minutesRemaining = Math.floor(timeRemaining / 60);
+  let secondsRemaining = timeRemaining % 60;
+
+  document.getElementById("draw-timer").innerHTML = "Next draw in " + minutesRemaining + " minutes and " + secondsRemaining + " seconds";
+}
+
+function performDraw() {
+  // randomly select winner
+  let winnerIndex = Math.floor(Math.random() * ticketsPurchased);
+  let winner = tickets[winnerIndex];
+  
+  // update pool total and tickets purchased
+  poolTotal = 0;
+  ticketsPurchased = 0;
+  document.getElementById("pool-total").innerHTML = "$" + poolTotal;
+  document.getElementById("tickets-purchased").innerHTML = ticketsPurchased;
+  
+  // clear tickets array
+  tickets = [];
+  
+  // update winner display
+  document.getElementById("winner").innerHTML = "The winner is " + winner;
+}
+
+// set up event listeners
+document.getElementById("buy-button").addEventListener("click", function() {
+  let numTickets = parseInt(document.getElementById("num-tickets").value);
+  buyTickets(numTickets);
+});
+
+document.getElementById("clear-button").addEventListener("click", function() {
+  clearTickets();
+});
+
+// set up draw timer
+updateDrawTime();
 setInterval(function() {
-  drawWinner();
-}, 600000); // 10 minutes in milliseconds
-
-function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10)
-    seconds = parseInt(timer % 60, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-      timer = duration;
-      drawWinner();
-    }
-  }, 1000);
-}
-
-window.onload = function () {
-  var tenMinutes = 60 * 10,
-      display = document.querySelector('#time');
-  startTimer(tenMinutes, display);
-};
+  updateDrawTime();
+  if (new Date().getSeconds() === 0) {
+    performDraw();
+  }
+}, 1000);
