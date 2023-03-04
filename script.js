04fd5b2fc089695bd555268864a3094feffa1680
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-03-04 17:39:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
-const ticketCount = document.getElementById("ticket-count");
-const ticketNum = document.getElementById("ticket-num");
-const addTicketBtn = document.getElementById("add-ticket-btn");
-const timerMinutes = document.getElementById("timer-minutes");
-const timerSeconds = document.getElementById("timer-seconds");
-const lastWinner = document.getElementById("last-winner");
-const winnerList = document.getElementById("winner-list");
-
 let totalTickets = 0;
-let countdown;
+let ticketList = [];
+let winnerList = [];
 
-function addTicket() {
-  const numTickets = parseInt(ticketNum.value);
-  if (numTickets > 0) {
-    totalTickets += numTickets;
-    ticketCount.innerText = totalTickets;
-    ticketNum.value = "";
+// Function to add tickets to pool
+function addTickets() {
+  const newTickets = parseInt(document.getElementById('ticketNumber').value);
+  if (!isNaN(newTickets)) {
+    totalTickets += newTickets;
+    ticketList.push(newTickets);
+    document.getElementById('ticketNumber').value = '';
+    updateTotalTickets();
   }
 }
 
-function countdownTimer() {
-  let minutes = 10;
-  let seconds = 0;
-  countdown = setInterval(() => {
-    if (seconds === 0) {
-      if (minutes === 0) {
-        drawWinner();
-        minutes = 10;
-      } else {
-        minutes--;
-        seconds = 59
+// Function to update the total ticket count on the page
+function updateTotalTickets() {
+  document.getElementById('totalTickets').innerHTML = totalTickets;
+}
+
+// Function to start the timer and schedule the drawing of winners
+function startTimer() {
+  const timeLeft = 10 * 60; // 10 minutes in seconds
+  let timeRemaining = timeLeft;
+  let intervalId = setInterval(() => {
+    let minutes = Math.floor(timeRemaining / 60);
+    let seconds = timeRemaining % 60;
+    document.getElementById('timer').innerHTML = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    timeRemaining--;
+    if (timeRemaining < 0) {
+      clearInterval(intervalId);
+      drawWinner();
+      startTimer();
+    }
+  }, 1000);
+}
+
+// Function to draw a winner and reset the timer
+function drawWinner() {
+  if (ticketList.length > 0) {
+    const winnerIndex = Math.floor(Math.random() * ticketList.length);
+    const winningTicket = ticketList.splice(winnerIndex, 1)[0];
+    winnerList.unshift(winningTicket);
+    if (winnerList.length > 10) {
+      winnerList.pop();
+    }
+    displayWinners();
+    totalTickets -= winningTicket;
+    updateTotalTickets();
+    document.getElementById('winner').innerHTML = `Last winner: ${winningTicket} tickets`;
+  }
+}
+
+// Function to display the list of past winners
+function displayWinners() {
+  let winnerHtml = '';
+  for (let i = 0; i < winnerList.length; i++) {
+    winnerHtml += `<li>${winnerList[i]} tickets</li>`;
+  }
+  document.getElementById('winnerList').innerHTML = winnerHtml;
+}
+
+document.getElementById('addTicketsButton').addEventListener('click', addTickets);
+startTimer();
