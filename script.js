92e121742540c617728b745f8e60b07d2e4e93eb
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-03-04 11:51:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,66 @@
-// Global Variables
-let ticketCount = 0;
-let poolTotal = 0;
-let drawTimer;
+// Initialize variables
+let totalTickets = 0;
+let ticketsInPool = 0;
+let ticketsThisRound = 0;
+let drawInterval = 600000; // 10 minutes in milliseconds
+let timeToDraw = drawInterval;
 
-// Function to add tickets to the pool
-function addTickets() {
-  let numTickets = parseInt(prompt("How many tickets would you like to add?"));
-  if (numTickets > 0) {
-    ticketCount += numTickets;
-    poolTotal += numTickets * 10;
-    updatePool();
+// Initialize HTML elements
+const buyBtn = document.querySelector(".buy-btn");
+const ticketCount = document.querySelector(".ticket-count");
+const totalPool = document.querySelector(".total-pool");
+const drawTimer = document.querySelector(".draw-timer");
+
+// Add event listener to buy button
+buyBtn.addEventListener("click", () => {
+  const input = prompt("How many tickets would you like to buy?");
+  const numTickets = parseInt(input);
+
+  // Verify input is a number
+  if (isNaN(numTickets)) {
+    alert("Please enter a valid number.");
+    return;
   }
+
+  // Add tickets to total ticket count
+  totalTickets += numTickets;
+
+  // Add tickets to pool and update tickets in pool display
+  ticketsInPool += numTickets;
+  ticketCount.textContent = ticketsInPool;
+
+  // Update total pool display
+  totalPool.textContent = `$${ticketsInPool}`;
+
+  // Clear ticket count for next round
+  ticketsThisRound = 0;
+});
+
+// Function to update draw timer
+function updateDrawTimer() {
+  // Decrement time to draw
+  timeToDraw -= 1000;
+
+  // Check if it's time to draw
+  if (timeToDraw <= 0) {
+    // Randomly select a winner
+    const winner = Math.floor(Math.random() * ticketsInPool) + 1;
+    alert(`Congratulations to ticket number ${winner}!`);
+
+    // Clear tickets and update displays
+    ticketsInPool = 0;
+    ticketCount.textContent = 0;
+    totalPool.textContent = "$0";
+
+    // Reset time to draw
+    timeToDraw = drawInterval;
+  }
+
+  // Update draw timer display
+  const minutes = Math.floor(timeToDraw / 60000);
+  const seconds = Math.floor((timeToDraw % 60000) / 1000);
+  drawTimer.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
 
-// Function to clear all tickets from the pool
-function clearTickets() {
-  ticketCount = 0;
-  poolTotal = 0;
-  updatePool();
-}
-
-// Function to update the pool total
-function updatePool() {
-  let poolTotalElem = document.querySelector(".pool-total");
-  poolTotalElem.innerText = "$" + poolTotal;
-}
-
-// Function to start the draw timer
-function startDrawTimer() {
-  let now = new Date();
-  let drawDate = new Date();
-  drawDate.setDate(now.getDate() + 7); // Set draw date to 7 days from now
-
-  let timeDiff = drawDate.getTime() - now.getTime();
-  let seconds = Math.floor(timeDiff / 1000);
-
-  drawTimer = setInterval(function () {
-    seconds--;
-    let timerElem = document.querySelector("#timer");
-    let minutes = Math.floor(seconds / 60);
-    let remainingSeconds = seconds % 60;
-    timerElem.innerText = minutes + "m " + remainingSeconds + "s";
-    if (seconds <= 0) {
-      clearInterval(drawTimer);
-      drawWinner();
-    }
-  }, 1000);
-}
-
-// Function to draw the winner
-function drawWinner() {
-  let winningTicket = Math.floor(Math.random() * ticketCount) + 1;
-  alert("Ticket #" + winningTicket + " wins the pool of $" + poolTotal);
-  clearTickets();
-  startDrawTimer();
-}
-
-// Start the draw timer on page load
-window.onload = function () {
-  startDrawTimer();
-};
+// Update draw timer every second
+setInterval(updateDrawTimer, 1000);
