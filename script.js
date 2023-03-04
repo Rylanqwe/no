88d38613073729fe9e0686db3996b787aa5816cf
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-03-04 16:18:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,62 @@
-// set up variables
-let ticketPrice = 1;
+// Set up variables
 let poolTotal = 0;
-let ticketsPurchased = 0;
-let tickets = [];
-let drawTime = 10; // minutes
+let ticketsSold = 0;
+let countdownIntervalId;
+let nextDrawTime = Date.now() + 10 * 60 * 1000; // 10 minutes from now
 
-// set up functions
-function buyTickets(numTickets) {
-  // add tickets to array
-  for (let i = 0; i < numTickets; i++) {
-    tickets.push("Ticket " + (ticketsPurchased + 1 + i));
+// Get elements from HTML
+const poolTotalElem = document.getElementById('pool-total');
+const ticketForm = document.getElementById('ticket-form');
+const buyBtn = document.getElementById('buy-btn');
+const winnerNameElem = document.getElementById('winner-name');
+const countdownTimerElem = document.getElementById('countdown-timer');
+
+// Function to handle buying tickets
+function buyTickets(event) {
+  event.preventDefault();
+  const numTickets = parseInt(document.getElementById('num-tickets').value);
+  if (numTickets) {
+    poolTotal += numTickets;
+    ticketsSold += numTickets;
+    updatePoolTotal();
+    document.getElementById('num-tickets').value = '';
   }
-  // update tickets purchased and pool total
-  ticketsPurchased += numTickets;
-  poolTotal += numTickets * ticketPrice;
-  document.getElementById("tickets-purchased").innerHTML = ticketsPurchased;
-  document.getElementById("pool-total").innerHTML = "$" + poolTotal;
 }
 
-function clearTickets() {
-  tickets = [];
-  ticketsPurchased = 0;
-  document.getElementById("tickets-purchased").innerHTML = ticketsPurchased;
+// Function to update pool total on HTML
+function updatePoolTotal() {
+  poolTotalElem.innerText = '$' + poolTotal;
 }
 
-function updateDrawTime() {
-  let currentTime = new Date();
-  let nextDrawTime = new Date();
-  nextDrawTime.setMinutes(Math.ceil(currentTime.getMinutes() / drawTime) * drawTime);
-  nextDrawTime.setSeconds(0);
-  nextDrawTime.setMilliseconds(0);
-
-  let timeRemaining = Math.ceil((nextDrawTime.getTime() - currentTime.getTime()) / 1000);
-
-  let minutesRemaining = Math.floor(timeRemaining / 60);
-  let secondsRemaining = timeRemaining % 60;
-
-  document.getElementById("draw-timer").innerHTML = "Next draw in " + minutesRemaining + " minutes and " + secondsRemaining + " seconds";
+// Function to choose a random winner
+function chooseWinner() {
+  const winnerNumber = Math.floor(Math.random() * ticketsSold) + 1;
+  winnerNameElem.innerText = 'Ticket ' + winnerNumber;
+  resetPool();
 }
 
-function performDraw() {
-  // randomly select winner
-  let winnerIndex = Math.floor(Math.random() * ticketsPurchased);
-  let winner = tickets[winnerIndex];
-  
-  // update pool total and tickets purchased
+// Function to reset pool and clear tickets
+function resetPool() {
   poolTotal = 0;
-  ticketsPurchased = 0;
-  document.getElementById("pool-total").innerHTML = "$" + poolTotal;
-  document.getElementById("tickets-purchased").innerHTML = ticketsPurchased;
-  
-  // clear tickets array
-  tickets = [];
-  
-  // update winner display
-  document.getElementById("winner").innerHTML = "The winner is " + winner;
+  ticketsSold = 0;
+  updatePoolTotal();
 }
 
-// set up event listeners
-document.getElementById("buy-button").addEventListener("click", function() {
-  let numTickets = parseInt(document.getElementById("num-tickets").value);
-  buyTickets(numTickets);
-});
-
-document.getElementById("clear-button").addEventListener("click", function() {
-  clearTickets();
-});
-
-// set up draw timer
-updateDrawTime();
-setInterval(function() {
-  updateDrawTime();
-  if (new Date().getSeconds() === 0) {
-    performDraw();
+// Function to update countdown timer
+function updateCountdown() {
+  const currentTime = Date.now();
+  const timeDifference = nextDrawTime - currentTime;
+  if (timeDifference <= 0) {
+    chooseWinner();
+    nextDrawTime = currentTime + 10 * 60 * 1000;
   }
-}, 1000);
+  const minutes = Math.floor(timeDifference / 60000);
+  const seconds = Math.floor((timeDifference % 60000) / 1000);
+  countdownTimerElem.innerText = minutes + 'm ' + seconds + 's';
+}
+
+// Add event listeners
+ticketForm.addEventListener('submit', buyTickets);
+
+// Start countdown timer
+countdownIntervalId = setInterval(updateCountdown, 1000);
