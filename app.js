e46037d5ee9e5433119397fc0d16c5a27b71fbc9
Rylanqwe <122843110+Rylanqwe@users.noreply.github.com>
2023-03-04 17:29:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,26 @@
-// Get elements from the DOM
-const addTicketBtn = document.getElementById('addTicketBtn');
-const ticketCountEl = document.getElementById('ticketCount');
-const timeLeftEl = document.getElementById('timeLeft');
-const listEl = document.getElementById('list');
+var ticketCount = 0;
+var tickets = [];
+var timer;
 
-// Variables to store state
-let ticketCount = 0;
-let winningTickets = [];
-
-// Add a ticket to the pool
 function addTicket() {
-  ticketCount++;
-  ticketCountEl.textContent = ticketCount;
+	var numTickets = parseInt(document.getElementById("numTickets").value);
+	for (var i = 0; i < numTickets; i++) {
+		tickets.push(++ticketCount);
+	}
+	document.getElementById("ticketCount").innerHTML = ticketCount;
+	document.getElementById("numTickets").value = "";
 }
 
-// Draw a winner and reset the ticket pool and timer
-function drawWinner() {
-  const winningTicket = Math.floor(Math.random() * ticketCount) + 1;
-  winningTickets.unshift(winningTicket);
-  if (winningTickets.length > 10) {
-    winningTickets.pop();
-  }
-  listEl.innerHTML = '';
-  winningTickets.forEach(ticket => {
-    const li = document.createElement('li');
-    li.textContent = ticket;
-    listEl.appendChild(li);
-  });
-  ticketCount = 0;
-  ticketCountEl.textContent = ticketCount;
-  startTimer();
-}
+function startTimer(duration, display) {
+	var timer = duration, minutes, seconds;
+	setInterval(function () {
+		minutes = parseInt(timer / 60, 10);
+		seconds = parseInt(timer % 60, 10);
 
-// Start the timer
-function startTimer() {
-  let timeLeft = 600; // 10 minutes in seconds
-  const timerInterval = setInterval(() => {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    timeLeftEl.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
-    timeLeft--;
-    if (timeLeft < 0) {
-      clearInterval(timerInterval);
-      drawWinner();
-    }
-  }, 1000);
-}
+		minutes = minutes < 10 ? "0" + minutes : minutes;
+		seconds = seconds < 10 ? "0" + seconds : seconds;
 
-// Add event listener to the add ticket button
-addTicketBtn.addEventListener('click', addTicket);
+		display.querySelector("#minutes").innerHTML = minutes;
+		display.querySelector("#seconds").innerHTML = seconds;
 
-// Start
+		if (--timer < 0
